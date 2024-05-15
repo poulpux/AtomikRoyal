844b33e6699d7c43929c403407930564ad49de29
Poulpux <114729806+poulpux@users.noreply.github.com>
2024-05-15 17:01:06 +0200

    Là ça devrait être bon

---
+++ Assets/Scripts/Bomb/BombUsableDefault.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombUsableDefault : BombUsable
+{
+    public override void Use(PlayerInfos playerInfos)
+    {
+        base.Use(playerInfos);
+    }
+}
