6c9a7001d58cf5cd3879d2a341aef7ebbda7b048
Poulpux <114729806+poulpux@users.noreply.github.com>
2024-05-16 16:46:27 +0200

    Allez, c'est parti

---
+++ Assets/Scripts/StaticClass/StaticRounds/StaticRoundsSO.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "StaticRoundsSO", menuName = "Static/StaticRoundsSO")]
+public class StaticRoundsSO : ScriptableObject
+{
+    //[Header("All GameMods")]
+    //[Space(10)]
+    //public List<UsableSO> allGameMods = new List<UsableSO>();
+}
