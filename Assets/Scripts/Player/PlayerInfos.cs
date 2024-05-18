275f124af4cc2d2376d778514b2190303d48673d
Poulpux <114729806+poulpux@users.noreply.github.com>
2024-05-18 14:15:22 +0200

    Le player info de bon

--- Assets/Scripts/Player/PlayerInfos.cs
+++ Assets/Scripts/Player/PlayerInfos.cs
@@ -5,11 +5,36 @@ using UnityEngine;
 
 public class PlayerInfos : MonoBehaviour
 {
+    [Header("Main infos")]
+    [Space(10)]
     public string pseudo;
     public int nbKill;
     public bool isDead;
     [SerializeField] private Collider2D colliderr;
     [HideInInspector] public Action<PlayerInfos> isDeadEvent;
+
+    [Header("Stats")]
+    [Space(10)]
+    //LIFE
+    public int currentLife;
+    public int maxLife;
+    //DAMAGES
+    public int dmgCAC, dmgBomb;
+    //OTHERS
+    public float spd, cdwThrow, throwForce, range, exploSize;
+
+    [Header("Stats points")]
+    [Space(10)]
+    public int spd_Stat;
+    public int maxLife_Stat, exploSize_Stat, dmgCAC_Stat, dmgBomb_Stat, cdwThrow_Stat, throwForce_Stat, range_Stat;
+
+    ////// Pourquoi pas ajouter les médailles ici aussi
+    /// 
+    /// 
+    /// 
+    /// 
+    /// 
+    //////
 
     void Start()
     {
@@ -22,8 +47,40 @@ public class PlayerInfos : MonoBehaviour
         
     }
 
+    public void UpgradeStat(PLAYERSTATS stats)
+    {
+        //TODO
+    }
+
+    public void IncreaseLife(int heal)
+    {
+        currentLife = currentLife + heal <= maxLife ? currentLife + heal : maxLife;
+    }
+
+    public void DecreaseLife(int damage)
+    {
+        currentLife -= damage;
+        if (currentLife <= 0)
+        {
+            isDead = true;
+            isDeadEvent.Invoke(this);
+        }
+    }
+
     private void GetAKill()
     {
         nbKill++;
+    }
+
+    private void SetAllStats()
+    {
+        spd = _StaticPlayer.GetValue(PLAYERSTATS.SPD, spd_Stat);
+        maxLife = (int)_StaticPlayer.GetValue(PLAYERSTATS.PVMAX, maxLife_Stat);
+        exploSize = _StaticPlayer.GetValue(PLAYERSTATS.EXPLOSIONSIZE, exploSize_Stat);
+        dmgCAC = (int)_StaticPlayer.GetValue(PLAYERSTATS.DMGCAC, dmgCAC_Stat);
+        dmgBomb = (int)_StaticPlayer.GetValue(PLAYERSTATS.DMGBOMB, dmgBomb_Stat);
+        cdwThrow = _StaticPlayer.GetValue(PLAYERSTATS.COOLDOWNTHROW, cdwThrow_Stat);
+        throwForce = _StaticPlayer.GetValue(PLAYERSTATS.THROWFORCE, throwForce_Stat);
+        range = _StaticPlayer.GetValue(PLAYERSTATS.RANGE, range_Stat);
     }
 }
