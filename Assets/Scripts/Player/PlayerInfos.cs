e5b258299bc5e9dbcf59093a0dbbfcb38974ad07
Poulpux <114729806+poulpux@users.noreply.github.com>
2024-05-18 16:14:33 +0200

    On relance pour voir si ça marche

--- Assets/Scripts/Player/PlayerInfos.cs
+++ Assets/Scripts/Player/PlayerInfos.cs
@@ -38,7 +38,7 @@ public class PlayerInfos : MonoBehaviour
 
     void Start()
     {
-        
+        SetAllStats();
     }
 
     
--- Assets/Scripts/StaticClass/StaticPlayer/@_StaticPlayer.cs
+++ Assets/Scripts/StaticClass/StaticPlayer/@_StaticPlayer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
+using static StaticPlayerSO;
 
 public enum PLAYERSTATS
 {
@@ -19,14 +20,15 @@ public static class _StaticPlayer
 {
     [Header("All curves")]
     [Space(10)]
-    static public AnimationCurve spdCostCurve;
-    static public AnimationCurve pvMaxCostCurve;
-    static public AnimationCurve explosionSizeCostCurve;
-    static public AnimationCurve dmgCaCCostCurve;
-    static public AnimationCurve dmgExplosionCostCurve;
-    static public AnimationCurve cooldownThrowCostCurve;
-    static public AnimationCurve grenadeSpdCost;
-    static public AnimationCurve rangeCostCurve;
+    //Courbe "nbAmélioration/prix"
+    static public UpgradeCurves spd;
+    static public UpgradeCurves pvMax;
+    static public UpgradeCurves explosionSize;
+    static public UpgradeCurves dmgCAC;
+    static public UpgradeCurves dmgBomb;
+    static public UpgradeCurves cdwThrow;
+    static public UpgradeCurves throwForce;
+    static public UpgradeCurves range;
 
     [Header("GeneralValues")]
     [Space(10)]
@@ -34,28 +36,49 @@ public static class _StaticPlayer
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    static public int GetPrice(PLAYERSTATS stats, int currentNbUpgrade)
+    static public int GetPrice(PLAYERSTATS stat, int currentNbUpgrade)
     {
-        //TODO
-        return 0;
+        return (int)GetUpgradeCurve(stat).CostCurve.Evaluate((float)currentNbUpgrade+1f);
     }
     
-    static public float GetValue(PLAYERSTATS stats, int currentNbUpgrade)
+    static public float GetValue(PLAYERSTATS stat, int currentNbUpgrade)
     {
-        //TODO
-        return 0f;
+        UpgradeCurves currentCurve = GetUpgradeCurve(stat);
+        return currentCurve.startValue + currentCurve.statsParLv * (float)currentNbUpgrade;
     }
 
 
     static public void Init(StaticPlayerSO SO)
     {
-        spdCostCurve = SO.spdCostCurve;
-        pvMaxCostCurve = SO.pvMaxCostCurve;
-        explosionSizeCostCurve = SO.explosionSizeCostCurve;
-        dmgCaCCostCurve = SO.dmgCaCCostCurve;
-        dmgExplosionCostCurve = SO.dmgExplosionCostCurve;
-        cooldownThrowCostCurve = SO.cooldownThrowCostCurve;
-        grenadeSpdCost = SO.grenadeSpdCost;
-        rangeCostCurve = SO.rangeCostCurve;
+        spd = SO.spd;
+        pvMax = SO.pvMax;
+        explosionSize = SO.explosionSize;
+        dmgCAC = SO.dmgCAC;
+        dmgBomb = SO.dmgBomb;
+        cdwThrow = SO.cdwThrow;
+        throwForce = SO.throwForce;
+        range = SO.range;
+    }
+
+    static private UpgradeCurves GetUpgradeCurve(PLAYERSTATS stat)
+    {
+        if (stat == PLAYERSTATS.SPD)
+            return spd;
+        else if(stat == PLAYERSTATS.PVMAX)
+            return pvMax;
+        else if(stat == PLAYERSTATS.EXPLOSIONSIZE)
+            return explosionSize;
+        else if(stat == PLAYERSTATS.DMGCAC)
+            return dmgCAC;
+        else if(stat == PLAYERSTATS.DMGBOMB)
+            return dmgBomb;
+        else if(stat == PLAYERSTATS.COOLDOWNTHROW)
+            return cdwThrow;
+        else if(stat == PLAYERSTATS.THROWFORCE)
+            return throwForce;
+        else if(stat == PLAYERSTATS.RANGE)
+            return range;
+
+        return null; 
     }
 }
