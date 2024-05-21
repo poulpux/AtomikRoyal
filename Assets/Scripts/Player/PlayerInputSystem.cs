aa89b7cd656c0bb491960dfb2d6b097f582b0c74
Poulpux <114729806+poulpux@users.noreply.github.com>
2024-05-21 16:56:22 +0200

    Update PlayerInputSystem.cs

--- Assets/Scripts/Player/PlayerInputSystem.cs
+++ Assets/Scripts/Player/PlayerInputSystem.cs
@@ -11,6 +11,12 @@ public enum CONFIG
 }
 public class PlayerInputSystem : MonoBehaviour
 {
+    //Toutes les variables interressantes
+    [HideInInspector] public UnityEvent isUsingUsableEvent = new UnityEvent(), isOpeningInventoryEvent = new UnityEvent(), isOpeningMapEvent = new UnityEvent();
+    [HideInInspector] public List<UnityEvent> inventoryEvent;
+    public Vector2 direction;
+    public bool isInteracting;
+
     class Button
     {
         public string name;
@@ -18,9 +24,8 @@ public class PlayerInputSystem : MonoBehaviour
     }
 
     PlayerInput playerInput;
-    [HideInInspector] public InputActionMap DefaultActions;
+    private InputActionMap DefaultActions;
     List<Button> buttonList  = new List<Button>();
-    public UnityEvent isUsingUsableEvent = new UnityEvent(), isOpeningInventoryEvent = new UnityEvent(), isOpeningMapEvent = new UnityEvent();
 
     void Awake()
     {
@@ -31,6 +36,7 @@ public class PlayerInputSystem : MonoBehaviour
 
         EnableActionMap(DefaultActions);
         SetAllButton();
+        SetButtonInventoryList();
     }
 
     private void Update()
@@ -46,6 +52,12 @@ public class PlayerInputSystem : MonoBehaviour
     {
     }
 
+    private void SetButtonInventoryList()
+    {
+        for (int i = 0; i < 6; i++)
+            inventoryEvent.Add(new UnityEvent());
+    }
+
     private void SwitchConfig(CONFIG config)
     {
         playerInput.currentActionMap.Disable();
@@ -55,27 +67,80 @@ public class PlayerInputSystem : MonoBehaviour
 
     private void EnableActionMap(InputActionMap action)
     {
+        action["Move"].performed += MoveAct;
+        action["Move"].canceled += MoveSleep;
+        action["Interact"].performed += InteractAct;
+        action["Interact"].canceled += InteractSleep;
+
+
+        //All buttons
         action["UseUsable"].started += UseUsableAct;
         action["UseUsable"].canceled += UseUsableSleep;
-        //action["Accelerate"].canceled += AccelerateSleep;
-        //action["Decelerate"].performed += DecelerateActing;
-        //action["Decelerate"].canceled += DecelerateSleep;
-        //action["Direction"].performed += GetDirectionActing;
-        //action["Direction"].canceled += GetDirectionSleep;
-        //action["Drift"].performed += TryDrift;
-        //action["Drift"].canceled += DriftSleep;
+        action["OpenInventory"].started += OpenInventoryAct;
+        action["OpenInventory"].canceled += OpenInventorySleep;
+        action["OpenMap"].started += OpenMapAct;
+        action["OpenMap"].canceled += OpenMapSleep;
+        action["Inventory1"].started += Inventory1Act;
+        action["Inventory1"].canceled += Inventory1Sleep;
+        action["Inventory2"].started += Inventory2Act;
+        action["Inventory2"].canceled += Inventory2Sleep;
+        action["Inventory3"].started += Inventory3Act;
+        action["Inventory3"].canceled += Inventory3Sleep;
+        action["Inventory4"].started += Inventory4Act;
+        action["Inventory4"].canceled += Inventory4Sleep;
+        action["Inventory5"].started += Inventory5Act;
+        action["Inventory5"].canceled += Inventory5Sleep;
+        action["Inventory6"].started += Inventory6Act;
+        action["Inventory6"].canceled += Inventory6Sleep;
     }
 
     private void DisableActionMap(InputActionMap action)
     {
-        //action["Accelerate"].performed -= AccelerateActing;
-        //action["Accelerate"].canceled -= AccelerateSleep;
-        //action["Decelerate"].performed -= DecelerateActing;
-        //action["Decelerate"].canceled -= DecelerateSleep;
-        //action["Direction"].performed -= GetDirectionActing;
-        //action["Direction"].canceled -= GetDirectionSleep;
-        //action["Drift"].performed -= TryDrift;
-        //action["Drift"].canceled -= DriftSleep;
+        action["Move"].performed -= MoveAct;
+        action["Move"].canceled -= MoveSleep;
+        action["Interact"].performed -= InteractAct;
+        action["Interact"].canceled -= InteractSleep;
+
+
+        //All buttons
+        action["UseUsable"].started -= UseUsableAct;
+        action["UseUsable"].canceled -= UseUsableSleep;
+        action["OpenInventory"].started -= OpenInventoryAct;
+        action["OpenInventory"].canceled -= OpenInventorySleep;
+        action["OpenMap"].started -= OpenMapAct;
+        action["OpenMap"].canceled -= OpenMapSleep;
+        action["Inventory1"].started -= Inventory1Act;
+        action["Inventory1"].canceled -= Inventory1Sleep;
+        action["Inventory2"].started -= Inventory2Act;
+        action["Inventory2"].canceled -= Inventory2Sleep;
+        action["Inventory3"].started -= Inventory3Act;
+        action["Inventory3"].canceled -= Inventory3Sleep;
+        action["Inventory4"].started -= Inventory4Act;
+        action["Inventory4"].canceled -= Inventory4Sleep;
+        action["Inventory5"].started -= Inventory5Act;
+        action["Inventory5"].canceled -= Inventory5Sleep;
+        action["Inventory6"].started -= Inventory6Act;
+        action["Inventory6"].canceled -= Inventory6Sleep;
+    }
+
+    private void MoveAct(InputAction.CallbackContext value)
+    {
+        direction = value.ReadValue<Vector2>();
+    }
+    
+    private void MoveSleep(InputAction.CallbackContext value)
+    {
+        direction = Vector2.zero;
+    }
+    
+    private void InteractAct(InputAction.CallbackContext value)
+    {
+        isInteracting = value.ReadValue<float>() > 0f;
+    }
+    
+    private void InteractSleep(InputAction.CallbackContext value)
+    {
+        isInteracting = false;
     }
 
     private void UseUsableAct(InputAction.CallbackContext value)
@@ -86,6 +151,84 @@ public class PlayerInputSystem : MonoBehaviour
     private void UseUsableSleep(InputAction.CallbackContext value)
     {
         ButtonSleep(0);
+    }
+
+    private void OpenInventoryAct(InputAction.CallbackContext value)
+    {
+        ButtonAct(1);
+    }
+    
+    private void OpenInventorySleep(InputAction.CallbackContext value)
+    {
+        ButtonSleep(1);
+    }
+    
+    private void OpenMapAct(InputAction.CallbackContext value)
+    {
+        ButtonAct(2);
+    }
+    
+    private void OpenMapSleep(InputAction.CallbackContext value)
+    {
+        ButtonSleep(2);
+    }
+    
+    private void Inventory1Act(InputAction.CallbackContext value)
+    {
+        ButtonAct(3);
+    }
+    
+    private void Inventory1Sleep(InputAction.CallbackContext value)
+    {
+        ButtonSleep(3);
+    }
+    private void Inventory2Act(InputAction.CallbackContext value)
+    {
+        ButtonAct(4);
+    }
+    
+    private void Inventory2Sleep(InputAction.CallbackContext value)
+    {
+        ButtonSleep(4);
+    }
+    private void Inventory3Act(InputAction.CallbackContext value)
+    {
+        ButtonAct(5);
+    }
+    
+    private void Inventory3Sleep(InputAction.CallbackContext value)
+    {
+        ButtonSleep(5);
+    }
+
+    private void Inventory4Act(InputAction.CallbackContext value)
+    {
+        ButtonAct(6);
+    }
+    
+    private void Inventory4Sleep(InputAction.CallbackContext value)
+    {
+        ButtonSleep(6);
+    }
+    
+    private void Inventory5Act(InputAction.CallbackContext value)
+    {
+        ButtonAct(7);
+    }
+    
+    private void Inventory5Sleep(InputAction.CallbackContext value)
+    {
+        ButtonSleep(7);
+    }
+    
+    private void Inventory6Act(InputAction.CallbackContext value)
+    {
+        ButtonAct(8);
+    }
+    
+    private void Inventory6Sleep(InputAction.CallbackContext value)
+    {
+        ButtonSleep(8);
     }
 
     private void ButtonAct(int index)
@@ -104,6 +247,7 @@ public class PlayerInputSystem : MonoBehaviour
     {
         SetButton("UseUsable");
         SetButton("OpenInventory");
+        SetButton("OpenMap");
         SetButton("Inventory1");
         SetButton("Inventory2");
         SetButton("Inventory3");
@@ -122,8 +266,14 @@ public class PlayerInputSystem : MonoBehaviour
     private UnityEvent GetEvent(int index)
     {
         if (index == 0)
-            return isUsingUsable;
-
-        return null;
+            return isUsingUsableEvent;
+        else if (index == 1)
+            return isOpeningInventoryEvent;
+        else if (index == 2)
+            return isOpeningMapEvent;
+        else
+        {
+            return inventoryEvent[index - 3];
+        }
     }
 }
