5c22bac8873deb1bc923893c3435ea7bd7caca80
Poulpux <114729806+poulpux@users.noreply.github.com>
2024-05-27 09:34:31 +0200

    On commence avec les scriptablesObject de l'infini

--- Assets/Scripts/Factory/UtilitySO/UtilityUsableSO.cs
+++ Assets/Scripts/Factory/UtilitySO/UtilityUsableSO.cs
@@ -1,15 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+
+public enum UTILITYTYPE
+{
+    HEAL,
+    SHIELD,
+    SPDBOOST,
+    GRENADECDW,
+    GRENADEIMPULSE,
+    MINE,
+    OTHER
+}
 
 [CreateAssetMenu(fileName = "UtilitySO_filename", menuName = "SO/UtilitySO")]
 public class UtilityUsableSO : UsableSO
 {
+    [SerializeField] UTILITYTYPE type;
+
+    [ConditionalField("type", UTILITYTYPE.HEAL, "==")]  public int nbHeal;
+    [ConditionalField("type", UTILITYTYPE.HEAL, "==")]  public float timeUse;
+
+
+
+    //Other
+    [ConditionalField("type", UTILITYTYPE.OTHER, "==")] public TextAsset otherScript;
+
+    private const string healScriptPath = "Assets/Scripts/Factory/UtilitySO/UtilityUsableHeal.cs";
+
     private void OnValidate()
     {
-        VerifType<UtilityUsable>();
+        if (type == UTILITYTYPE.HEAL)
+            script = AssetDatabase.LoadAssetAtPath<TextAsset>(healScriptPath);
+        else
+            VerifOtherType<UtilityUsable>();
     }
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    protected void VerifOtherType<T>()
+    {
+        if (otherScript == null)
+            throw new System.Exception("No reference on " + typeof(T) + " script");
+
+        System.Type t = System.Type.GetType(otherScript.name.Replace(".cs", ""));
+
+        if (t == null)
+        {
+            otherScript = null;
+            throw new System.Exception("The referenced asset is not a script");
+        }
+
+        if (t.BaseType != typeof(T))
+        {
+            otherScript = null;
+            throw new System.Exception("The referenced script is not an " + typeof(T));
+        }
+
+        script = otherScript;
+    }
 }
