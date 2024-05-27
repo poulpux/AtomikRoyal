71028387f8b2c52e2ae24ee95996a092e6e1dbc5
Poulpux <114729806+poulpux@users.noreply.github.com>
2024-05-27 10:13:51 +0200

    Instantiation du usableSO

--- Assets/Scripts/Factory/UtilitySO/UtilityUsableSO.cs
+++ Assets/Scripts/Factory/UtilitySO/UtilityUsableSO.cs
@@ -18,22 +18,45 @@ public enum UTILITYTYPE
 [CreateAssetMenu(fileName = "UtilitySO_filename", menuName = "SO/UtilitySO")]
 public class UtilityUsableSO : UsableSO
 {
+    [Header("TYPE")]
+    [Space(10)]
     [SerializeField] UTILITYTYPE type;
 
     [ConditionalField("type", UTILITYTYPE.HEAL, "==")]  public int nbHeal;
-    [ConditionalField("type", UTILITYTYPE.HEAL, "==")]  public float timeUse;
+    [ConditionalField("type", UTILITYTYPE.SHIELD, "==")]  public int nbShield;
+    [ConditionalField("type", UTILITYTYPE.SPDBOOST, "==")]  public int spdModifier;
+    [ConditionalField("type", UTILITYTYPE.GRENADECDW, "!=")][ConditionalField("type", UTILITYTYPE.GRENADEIMPULSE, "!=")][ConditionalField("type", UTILITYTYPE.MINE, "!=")][ConditionalField("type", UTILITYTYPE.OTHER, "!=")]  
+    public float timeUse;
 
+    [ConditionalField("type", UTILITYTYPE.GRENADECDW, "==")] public float cdw;
 
+    [ConditionalField("type", UTILITYTYPE.HEAL, "!=")] [ConditionalField("type", UTILITYTYPE.SHIELD, "!=")] [ConditionalField("type", UTILITYTYPE.SPDBOOST, "!=")][ConditionalField("type", UTILITYTYPE.OTHER, "!=")]
+    public GameObject objectToInstantiate, explosionPrefab;
 
     //Other
     [ConditionalField("type", UTILITYTYPE.OTHER, "==")] public TextAsset otherScript;
 
     private const string healScriptPath = "Assets/Scripts/Factory/UtilitySO/UtilityUsableHeal.cs";
+    private const string shieldScriptPath = "Assets/Scripts/Factory/UtilitySO/UtilityUsableShield.cs";
+    private const string spdBoostScriptPath = "Assets/Scripts/Factory/UtilitySO/UtilityUsableSpdBoost.cs";
+    private const string grenadeCDWScriptPath = "Assets/Scripts/Factory/UtilitySO/UtilityUsableGrenadeCDW.cs";
+    private const string grenadeImpulseScriptPath = "Assets/Scripts/Factory/UtilitySO/UtilityUsableGrenadeCDW.cs";
+    private const string mineScriptPath = "Assets/Scripts/Factory/UtilitySO/UtilityUsableMine.cs";
 
     private void OnValidate()
     {
         if (type == UTILITYTYPE.HEAL)
             script = AssetDatabase.LoadAssetAtPath<TextAsset>(healScriptPath);
+        else if (type == UTILITYTYPE.SHIELD)
+            script = AssetDatabase.LoadAssetAtPath<TextAsset>(shieldScriptPath);
+        else if (type == UTILITYTYPE.SPDBOOST)
+            script = AssetDatabase.LoadAssetAtPath<TextAsset>(spdBoostScriptPath);
+        else if (type == UTILITYTYPE.GRENADECDW)
+            script = AssetDatabase.LoadAssetAtPath<TextAsset>(grenadeCDWScriptPath);        
+        else if (type == UTILITYTYPE.GRENADEIMPULSE)
+            script = AssetDatabase.LoadAssetAtPath<TextAsset>(grenadeImpulseScriptPath);
+        else if (type == UTILITYTYPE.MINE)
+            script = AssetDatabase.LoadAssetAtPath<TextAsset>(mineScriptPath);
         else
             VerifOtherType<UtilityUsable>();
     }
