89ff680c97ee1e2678a184290c1c5cba55e9eeb1
Poulpux <114729806+poulpux@users.noreply.github.com>
2024-05-29 12:29:51 +0200

    Update BombUsableSO.cs

--- Assets/Scripts/Items/OnInventory/Archi/Bomb/BombUsableSO.cs
+++ Assets/Scripts/Items/OnInventory/Archi/Bomb/BombUsableSO.cs
@@ -7,8 +7,7 @@ public enum BOMBTYPE
 {
     GRENADECDW,
     GRENADEIMPULSE,
-    MINE,
-    OTHER
+    MINE
 }
 
 [CreateAssetMenu(fileName = "BombSO_fileName", menuName = "SO/BombSO")]
@@ -21,13 +20,8 @@ public class BombUsableSO : UsableSOMother
 
     [ConditionalField("type", BOMBTYPE.GRENADECDW, "==")] public float cdw;
     [HideInInspector] public PlayerInfos owner;
-    [ConditionalField("type", BOMBTYPE.OTHER, "!=")]
     public Sprite objectToInstantiate;
-    [ConditionalField("type", BOMBTYPE.OTHER, "!=")]
     public GameObject explosionPrefab;
-    [ConditionalField("type", BOMBTYPE.OTHER, "==")]
-    public TextAsset otherScript;
-
     //[SerializeField] private TextAsset bombScript;
 
     public const string prefabPathGrenadeImpulse = "PrefabGrenadeImpulse";
@@ -40,9 +34,6 @@ public class BombUsableSO : UsableSOMother
     private void OnValidate()
     {
         script = AssetDatabase.LoadAssetAtPath<TextAsset>(bombScriptPath);
-        if(type == BOMBTYPE.OTHER)
-            VerifOtherType<UsableMother>();
-
         VerifExplosionPrefabComponent();
     }
 
@@ -70,27 +61,5 @@ public class BombUsableSO : UsableSOMother
             explosionPrefab = null;
             throw new System.Exception("explosion prefab haven't the script Explosion");
         }
-    }
-
-    protected void VerifOtherType<T>()
-    {
-        if (otherScript == null)
-            throw new System.Exception("No reference on " + typeof(T) + " script");
-
-        System.Type t = System.Type.GetType(otherScript.name.Replace(".cs", ""));
-
-        if (t == null)
-        {
-            otherScript = null;
-            throw new System.Exception("The referenced asset is not a script");
-        }
-
-        if (t.BaseType != typeof(T))
-        {
-            otherScript = null;
-            throw new System.Exception("The referenced script is not an " + typeof(T));
-        }
-
-        script = otherScript;
     }
 }
