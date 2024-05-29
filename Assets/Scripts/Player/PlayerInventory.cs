cfd20edd6974bcb74670bbc63ddc61b967292e83
Poulpux <114729806+poulpux@users.noreply.github.com>
2024-05-29 17:26:09 +0200

    sa

--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -7,10 +7,32 @@ using UnityEngine;
 public class PlayerInventory : MonoBehaviour, IDesactiveWhenPlayerIsDead
 {
     PlayerInfos infos;
-    public int cursorPos, nbCoins;
-    public List<UsableMother> Inventory = new List<UsableMother>();
-    public List<int> nbInInventory = new List<int>();
-    public List<string> cantUseItem = new List<string>();
+    public int cursorPos { get; private set; }
+    public int  nbCoins { get; private set; }
+
+    private List<UsableMother> _inventory = new List<UsableMother>();
+    private List<int> _nbInInventory = new List<int>();
+    private List<string> _cantUseItem = new List<string>();
+
+    // Public getters, private setters
+    public List<UsableMother> inventory
+    {
+        get { return _inventory; }
+        private set { _inventory = value; }
+    }
+
+    public List<int> nbInInventory
+    {
+        get { return _nbInInventory; }
+        private set { _nbInInventory = value; }
+    }
+
+    public List<string> cantUseItem
+    {
+        get { return _cantUseItem; }
+        private set { _cantUseItem = value; }
+    }
+
     void Start()
     {
         infos = GetComponent<PlayerInfos>();
@@ -19,9 +41,9 @@ public class PlayerInventory : MonoBehaviour, IDesactiveWhenPlayerIsDead
         infos.inputSystem.isUsingUsableEvent.AddListener(() => UseItem());
 
         for (int i = 0; i < _StaticPlayer.nbCasesInventory; i++)
-            Inventory.Add(null);
+            _inventory.Add(null);
         for (int i = 0; i < _StaticPlayer.nbCasesInventory; i++)
-            nbInInventory.Add(0);
+            _nbInInventory.Add(0);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -47,8 +69,8 @@ public class PlayerInventory : MonoBehaviour, IDesactiveWhenPlayerIsDead
     }
     public void UseItem()
     {
-        if (Inventory[cursorPos] == null || cantUseItem.Count != 0) return;
-            Inventory[cursorPos].TryUse();
+        if (_inventory[cursorPos] == null || _cantUseItem.Count != 0) return;
+            _inventory[cursorPos].TryUse();
     }
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -58,11 +80,11 @@ public class PlayerInventory : MonoBehaviour, IDesactiveWhenPlayerIsDead
         List<string> itemNames = new List<string>();
         itemNames.Add(SO.name);
         int counter = 0;
-        foreach (var item in Inventory)
+        foreach (var item in _inventory)
         {
             if(item == null) continue;
             print(SO.name);
-            if (nbInInventory[counter] != item.SO.nbMaxInventory  && item.SO.name == SO.name)
+            if (_nbInInventory[counter] != item.SO.nbMaxInventory  && item.SO.name == SO.name)
             {
                 index = counter;
                 return true;
@@ -75,7 +97,7 @@ public class PlayerInventory : MonoBehaviour, IDesactiveWhenPlayerIsDead
     private bool CheckIfVoidCase()
     {
         int index = 0;
-        foreach (var item in Inventory)
+        foreach (var item in _inventory)
         {
             if (item == null)
                 return true;
@@ -98,15 +120,15 @@ public class PlayerInventory : MonoBehaviour, IDesactiveWhenPlayerIsDead
 
     private void CompleteACase(UsableSOMother SO, int nb, int index, UsableOnGround usableOnGround)
     {
-        if (nbInInventory[index] + nb <= SO.nbMaxInventory)
+        if (_nbInInventory[index] + nb <= SO.nbMaxInventory)
         {
             usableOnGround.nb = 0;
-            nbInInventory[index] += nb;
+            _nbInInventory[index] += nb;
         }
         else
         {
-            int place = SO.nbMaxInventory - nbInInventory[index]; //positif
-            nbInInventory[index] = SO.nbMaxInventory;
+            int place = SO.nbMaxInventory - _nbInInventory[index]; //positif
+            _nbInInventory[index] = SO.nbMaxInventory;
             usableOnGround.nb = nb - place;
         }
        
@@ -115,10 +137,10 @@ public class PlayerInventory : MonoBehaviour, IDesactiveWhenPlayerIsDead
     private void AddInVoidCases(UsableSOMother SO, int nb, UsableOnGround usableOnGround)
     {
         int index = GetIndexVoidCase();
-        Inventory[index] = GF.SetScripts<UsableMother>(SO.script, gameObject);
-        nbInInventory[index] = nb <= SO.nbMaxInventory ? nb : SO.nbMaxInventory;
-        Inventory[index].UseEvent.AddListener(() => SubstractOneItem(index));
-        Inventory[index].SO = SO;
+        _inventory[index] = GF.SetScripts<UsableMother>(SO.script, gameObject);
+        _nbInInventory[index] = nb <= SO.nbMaxInventory ? nb : SO.nbMaxInventory;
+        _inventory[index].UseEvent.AddListener(() => SubstractOneItem(index));
+        _inventory[index].SO = SO;
 
         usableOnGround.nb = 0;
 
@@ -127,7 +149,7 @@ public class PlayerInventory : MonoBehaviour, IDesactiveWhenPlayerIsDead
     private int GetIndexVoidCase()
     {
         int index = 0;
-        foreach (var item in Inventory)
+        foreach (var item in _inventory)
         {
             if(item == null)
                 return index;
@@ -138,32 +160,32 @@ public class PlayerInventory : MonoBehaviour, IDesactiveWhenPlayerIsDead
 
     private void SubstractOneItem(int index)
     {
-        nbInInventory[index] -= 1;
-        if (nbInInventory[index] <= 0)
+        _nbInInventory[index] -= 1;
+        if (_nbInInventory[index] <= 0)
         {
-            Inventory[index].UseEvent.RemoveAllListeners();
+            _inventory[index].UseEvent.RemoveAllListeners();
             DestroyCurrentItem(index);
         }
     }
 
     private void EchangeInventoryItem(UsableSOMother SO, int nb)
     {
-        GameObject objet = _StaticChest.InstantiateUsable(transform.position, Inventory[cursorPos].SO, nbInInventory[cursorPos]);
+        GameObject objet = _StaticChest.InstantiateUsable(transform.position, _inventory[cursorPos].SO, _nbInInventory[cursorPos]);
         UsableOnGround onGround = objet.GetComponent<UsableOnGround>();
-        onGround.nb = nbInInventory[cursorPos];
-        onGround.SO = Inventory[cursorPos].SO;
+        onGround.nb = _nbInInventory[cursorPos];
+        onGround.SO = _inventory[cursorPos].SO;
 
-        Destroy(Inventory[cursorPos]);
-        Inventory[cursorPos] = GF.SetScripts<UsableMother>(SO.script, gameObject);
-        Inventory[cursorPos].SO = SO;
-        nbInInventory[cursorPos] = nb <= SO.nbMaxInventory ? nb : SO.nbMaxInventory;
+        Destroy(_inventory[cursorPos]);
+        _inventory[cursorPos] = GF.SetScripts<UsableMother>(SO.script, gameObject);
+        _inventory[cursorPos].SO = SO;
+        _nbInInventory[cursorPos] = nb <= SO.nbMaxInventory ? nb : SO.nbMaxInventory;
     }
 
 
     private void DestroyCurrentItem(int index)
     {
-        Destroy(Inventory[index]);
-        Inventory[index] = null;
+        Destroy(_inventory[index]);
+        _inventory[index] = null;
     }
 
     private void SetCursorEvent()
@@ -177,7 +199,7 @@ public class PlayerInventory : MonoBehaviour, IDesactiveWhenPlayerIsDead
 
     public void WhenDead()
     {
-        foreach (var item in Inventory)
+        foreach (var item in _inventory)
             Destroy(item);
         enabled = false;
     }
