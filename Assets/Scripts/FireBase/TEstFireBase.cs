35b3a408286297f939ab5e5d83ef0e57c212323a
Poulpux <114729806+poulpux@users.noreply.github.com>
2024-06-03 16:50:24 +0200

    Update TEstFireBase.cs

--- Assets/Scripts/FireBase/TEstFireBase.cs
+++ Assets/Scripts/FireBase/TEstFireBase.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TEstFireBase : MonoBehaviour
+public class TestFireBase : MonoBehaviour
 {
     public dataToSave dts;
     public nbFamililier familier;
