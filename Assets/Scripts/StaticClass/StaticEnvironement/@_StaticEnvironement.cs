4b53933027a3f587da795c8889e9695c6ab13dd3
Poulpux <114729806+poulpux@users.noreply.github.com>
2024-06-20 10:19:22 +0200

    Le static marche

---
+++ Assets/Scripts/StaticClass/StaticEnvironement/@_StaticEnvironement.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class _StaticEnvironement 
+{
+
+    static public void Init(StaticEnvironementSO SO)
+    {
+
+    }
+}
---
+++ Assets/Scripts/StaticClass/StaticEnvironement/StaticEnvironementSO.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum ELEMENTS
+{
+    UNBREAKABLEWALL,
+    BREAKABLEWALL,
+    FLAMMABLEWALL,
+    WATER,
+    BUSH,
+    GLUE,
+    GAZ,
+    FIRE,
+    SMOKE
+}
+
+[CreateAssetMenu(fileName = "StaticEnvironementSO", menuName = "Static/StaticEnvironementSO")]
+public class StaticEnvironementSO : ScriptableObject
+{
+     
+
+}
