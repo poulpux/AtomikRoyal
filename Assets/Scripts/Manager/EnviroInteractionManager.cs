cbb52d43e30f5b5e683adf9affd71b575bbd00fd
Poulpux <114729806+poulpux@users.noreply.github.com>
2024-06-24 15:39:14 +0200

    DUcoup le manager est là

--- Assets/Scripts/Manager/EnviroInteractionManager.cs
+++ Assets/Scripts/Manager/EnviroInteractionManager.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class EnviroInteractionManager : SingletonMother<EnviroInteractionManager>
 {
-    public int[,] binaryMaskMap { get; private set; } = new int[5, 5];
+    public int[,] binaryMaskMap { get; private set; }
     public void Start()
     {
-        
+        binaryMaskMap = new int[_StaticEnvironement.mapLenght / _StaticEnvironement.tabResolution, _StaticEnvironement.mapLenght / _StaticEnvironement.tabResolution];
+        print(binaryMaskMap[5, 5]);
     }
 }
